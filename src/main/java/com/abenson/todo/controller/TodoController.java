9233d988db790879c51d7180d8692352a7c35513
Andrew Benson <andrewbenson123@gmail.com>
2026-02-17 20:10:00 -0500

    Added simple logging to controller and service layers

--- src/main/java/com/abenson/todo/controller/TodoController.java
+++ src/main/java/com/abenson/todo/controller/TodoController.java
@@ -2,24 +2,35 @@ package com.abenson.todo.controller;
 
 import com.abenson.todo.model.TodoDTO;
 import com.abenson.todo.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class TodoController {
 
+    // The service to handle todo operations, injected by Spring
     private final TodoService todoService;
 
-
+    /**
+     * Constructor for TodoController
+     * @param todoService The service to handle todo operations, injected by Spring
+     */
     public TodoController(@Autowired TodoService todoService) {
         this.todoService = todoService;
     }
 
 
+    /**
+     * Get the status of the API
+     * @return A message indicating that the API is running
+     */
     @GetMapping("/status")
     public ResponseEntity<String> getStatus() {
+        log.info("Received request for API status");
         return ResponseEntity.ok("Todo API is running!");
     }
 
@@ -30,6 +41,7 @@ public class TodoController {
      */
     @GetMapping("/todo")
     public ResponseEntity<?> getAllTodos() {
+        log.info("Received request for API todos");
         return ResponseEntity.ok(todoService.findAll());
     }
 
@@ -40,17 +52,19 @@ public class TodoController {
         */
         @GetMapping("/todo/{id}")
     public ResponseEntity<?> getTodoById(@PathVariable("id") Long id) {
+            log.info("Received request for API todo with id: {}", id);
         return ResponseEntity.ok(todoService.findById(id));
     }
 
 
     /**
      * Delete a todo by id
-     * @param id
+     * @param id The id of the todo to delete
      * @return 204 No Content if the todo was deleted successfully, or 404 Not Found if the todo with the specified id does not exist
      */
     @DeleteMapping("/todo/{id}")
     public ResponseEntity<?> deleteTodoById(@PathVariable("id") Long id) {
+        log.info("Received request to delete API todo with id: {}", id);
         todoService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
@@ -62,7 +76,7 @@ public class TodoController {
      */
     @PostMapping("/todo")
     public ResponseEntity<?> createTodo(@RequestBody TodoDTO todo) {
-        System.out.println("In create todo controller method with todo: " + todo);
+        log.info("Received request to create API todo: {}", todo);
         return ResponseEntity.ok(todoService.saveTodo(todo));
     }
 
@@ -74,6 +88,7 @@ public class TodoController {
      */
     @PutMapping("/todo/{id}")
     public ResponseEntity<?> updateTodo(@PathVariable("id") Long id, @RequestBody TodoDTO todo) {
+        log.info("Received request to update API todo with id: {} and data: {}", id, todo);
         return ResponseEntity.ok(todoService.updateTodo(id, todo));
     }
 
--- src/main/java/com/abenson/todo/service/TodoServiceImpl.java
+++ src/main/java/com/abenson/todo/service/TodoServiceImpl.java
@@ -6,21 +6,31 @@ import com.abenson.todo.mappers.TodoMapper;
 import com.abenson.todo.model.Todo;
 import com.abenson.todo.model.TodoDTO;
 import com.abenson.todo.repository.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service implementation for managing todos
+ */
 @Service
+@Slf4j
 public class TodoServiceImpl implements TodoService {
 
     private final TodoRepository todoRepository;
     private final TodoMapper todoMapper;
 
+    /**
+     * Constructor for TodoServiceImpl
+     * @param todoRepository The repository to manage todo data, injected by Spring
+     * @param todoMapper The mapper to convert between Todo and TodoDTO, injected by Spring
+     */
    public TodoServiceImpl(@Autowired TodoRepository todoRepository, @Autowired TodoMapper todoMapper) {
-
        this.todoRepository = todoRepository;
        this.todoMapper = todoMapper;
    }
@@ -28,7 +38,10 @@ public class TodoServiceImpl implements TodoService {
     @Override
     public List<TodoDTO> findAll() {
 
-       return todoRepository.findAll().stream().map(todoMapper::toDto).toList();
+       log.info("Find all todos");
+       List<TodoDTO> todos = todoRepository.findAll().stream().map(todoMapper::toDto).toList();
+       log.info("Found {} todos", todos.size());
+       return todos;
     }
 
     @Override
@@ -36,6 +49,7 @@ public class TodoServiceImpl implements TodoService {
 
        Optional<Todo> optTodo = todoRepository.findById(id);
        if(optTodo.isPresent()) {
+           log.debug("Found todo with id: {}", id);
               return todoMapper.toDto(optTodo.get());
        }else{
               throw new ResourceNotFoundException("Resource not found with id: " + id);
@@ -45,18 +59,19 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public TodoDTO saveTodo(TodoDTO todoDto) {
-
-       System.out.println("Saving todo: " + todoDto);
        Todo todo = todoMapper.toEntity(todoDto);
 
        Optional<Todo> optTodo = todoRepository.findByTitleAndDescription(todo.getTitle(), todo.getDescription());
          if(optTodo.isPresent()) {
+             log.warn("Todo with the same title and description already exists");
              throw new ResourceAlreadyExistsException("Todo with the same title and description already exists");
          }
+
 
          todo.setCompleted(false);
          todo.setCreatedAt(LocalDateTime.now());
          Todo savedTodo = todoRepository.save(todo);
+         log.info("Todo with id: {} created successfully", savedTodo.getId());
 
         return todoMapper.toDto(savedTodo);
     }
@@ -65,8 +80,10 @@ public class TodoServiceImpl implements TodoService {
     public void deleteById(Long id) {
         Optional<Todo> optTodo = todoRepository.findById(id);
         if(optTodo.isPresent()) {
+            log.warn("Deleting todo with id: {}", id);
             todoRepository.deleteById(id);
         }else{
+            log.warn("Todo with id: {} not found for deletion", id);
             throw new ResourceNotFoundException("Resource not found with id: " + id);
         }
 
@@ -75,12 +92,13 @@ public class TodoServiceImpl implements TodoService {
     @Override
     public TodoDTO updateTodo(Long id, TodoDTO todoDto) {
        Todo todo = todoMapper.toEntity(todoDto);
-       System.out.println("Updating todo with id: " + id + " and data: " + todo);
+
 
         Optional<Todo> optTodo = todoRepository.findById(id);
         if(optTodo.isPresent()) {
-            Todo existingTodo = optTodo.get();
 
+            Todo existingTodo = optTodo.get();
+            log.info("Updating todo with id: {}", id);
             if(todo.getTitle() != null)
                 existingTodo.setTitle(todo.getTitle());
 
@@ -91,8 +109,10 @@ public class TodoServiceImpl implements TodoService {
                 existingTodo.setCompleted(todo.getCompleted());
 
             todoRepository.save(existingTodo );
+            log.info("Todo with id: {} updated successfully", id);
             return todoMapper.toDto(existingTodo);
         }else{
+            log.warn("Todo with id: {} not found for update", id);
             throw new ResourceNotFoundException("Resource not found with id: " + id);
         }
     }
